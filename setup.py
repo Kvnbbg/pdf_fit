de8c3eed9ff47bd77a03b8f787d206130a35b662
Kevin Marville <kvnbbg@gmail.com>
2025-03-24 10:50:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='pdf_size_reducer',
     version='1.0',
     author='Kevin Marville',
-    author_email='your_email@example.com',
+    author_email='kevinmarville@gmail.com',
     description='PDF Size Reducer App',
     app=APP,
     data_files=DATA_FILES,
